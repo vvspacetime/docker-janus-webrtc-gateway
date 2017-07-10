32b98f4c8b1e2e08bbd3447139fd48dbce03490f
spacetime <jingweiming-sp@outlook.com>
2017-07-10 12:05:27 +0800

    switch branch

--- janus.sh
+++ janus.sh
@@ -1,6 +1,7 @@
 cd ~
 git clone https://jingweiming@git.learning-tech.cn/realtimecat/janus-gateway.git
 cd janus-gateway
+git checkout shishimao
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs
 make

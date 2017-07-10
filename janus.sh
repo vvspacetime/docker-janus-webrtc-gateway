9b301b7034b61084ec7b20f1e92880a91d9002ca
spacetime <jingweiming-sp@outlook.com>
2017-07-10 10:45:49 +0800

    change url

--- janus.sh
+++ janus.sh
@@ -1,5 +1,5 @@
 cd ~
-git clone https://github.com/meetecho/janus-gateway.git
+git clone git@git.learning-tech.cn:realtimecat/janus-gateway.git
 cd janus-gateway
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs

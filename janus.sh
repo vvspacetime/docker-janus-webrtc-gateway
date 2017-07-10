f6fc34c0b3204cc2046f098617e58f781a1bdf3e
spacetime <jingweiming-sp@outlook.com>
2017-07-10 11:25:55 +0800

    pub

--- janus.sh
+++ janus.sh
@@ -1,5 +1,5 @@
 cd ~
-git clone git@git.learning-tech.cn:realtimecat/janus-gateway.git
+git clone https://jingweiming@git.learning-tech.cn/realtimecat/janus-gateway.git
 cd janus-gateway
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs

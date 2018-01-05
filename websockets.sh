a49b8a0d8e448aa466fac994546150475fa37f29
spacetime <jingweiming-sp@outlook.com>
2018-01-05 15:38:30 +0800

    update websocket

--- websockets.sh
+++ websockets.sh
@@ -1,7 +1,7 @@
 cd ~
 git clone https://github.com/warmcat/libwebsockets.git
 cd libwebsockets
-git checkout v2.1.0
+git checkout v2.4-stable
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..

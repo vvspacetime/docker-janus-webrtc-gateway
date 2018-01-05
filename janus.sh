dcceea4755878c818ef0b42380800e186f05ddda
spacetime <jingweiming-sp@outlook.com>
2018-01-05 14:01:36 +0800

    janus commit 8bfa9a3

--- janus.sh
+++ janus.sh
@@ -1,7 +1,7 @@
 cd ~
 git clone https://jingweiming@git.learning-tech.cn/realtimecat/janus-gateway.git
 cd janus-gateway
-git checkout shishimao
+git checkout 8bfa9a3
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt  --enable-post-processing
 make
